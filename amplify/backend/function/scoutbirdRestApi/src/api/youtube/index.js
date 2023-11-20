ed151455c3efb612b007b1af983a55fd533c9abb
JPereyraScoutBird <jpereyra@scoutbird.com>
2023-11-20 08:09:59 -0500

    added youtube date params

--- amplify/backend/function/scoutbirdRestApi/src/api/youtube/index.js
+++ amplify/backend/function/scoutbirdRestApi/src/api/youtube/index.js
@@ -1,22 +1,26 @@
 "use strict";
 
 const { default: axios } = require("axios");
-
+const moment = require("moment")
 const dayjs = require("dayjs");
 
 const baseUrl = "https://h3wumwtddd.execute-api.us-east-1.amazonaws.com/v1/";
 
 module.exports = async () => {
   return {
-    search: async (ticker) => {
-      const path = "youtube-features";
+    search: async (ticker, date_from = undefined, date_to = undefined) => {
       const currentDate = dayjs();
+      const from = date_from ? moment(date_from, "MM/DD/YYYY").format("YYYY-MM-DD") : currentDate.subtract(1, "month").format("YYYY-MM-DD")
+      const to =  date_to ? moment(date_to, "MM/DD/YYYY").format("YYYY-MM-DD") : currentDate.format("YYYY-MM-DD")
+      const path = "youtube-features";
+      console.log("From: ", from)
+      console.log("To: ", to)
       const {
         data: { statusCode, body },
       } = await axios.post(`${baseUrl}${path}`, {
         ticker,
-        from: currentDate.subtract(1, "month").format("YYYY-MM-DD"),
-        to: currentDate.format("YYYY-MM-DD"),
+        from,
+        to
       });
       return body;
     },
